f23a6d934913cb5fefa0fd1c1560651f6f8dc165
Omar Asael Hernandez Montiel <omarasael@nube.unadmexico.mx>
2023-03-23 23:18:33 -0700

    Cambio de principal de biblioteca

--- vistas/biblioteca/bprincipal.php
+++ vistas/biblioteca/bprincipal.php
@@ -66,10 +66,105 @@ $estadisticas =  buscaEstadisticasPrestamos();?>
       
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 ">
           <!--contenedor central -->
-<div class="row"><h1 class="text-center">Biblioteca</h1></div>
-<div class="row"><h1 class="text-center">proximamente catalogo</h1></div>
+
+
+
+<!-- aqui -->
+  <!--Mostrar prestamos vigentes --> 
+                         <h3 class="text-center">Préstamos vigentes por grupo</h3>
+                          <br>
+                            <div class="">
+                                        <!--combobox para seleccionar  grupo-->
+                                    <form class="form-control" action="bprincipal.php" method="post">
+                                            <div class="form-group">
+                                                        <div class="row">
+                                                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                                                <h6 class="text-center"><b>Selecciona Grupo:</b></h6>
+                                                            </div>
+                                                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                                                    <select name="grupo" class="form-control">
+                                                                    <?php foreach($grupos as $g):?>
+                                                                        
+                                                                    <option value="<?=$g->idgrupos?>"
+                                                                        <?php if(isset($grupoElegido)){
+                                                                            if($grupoElegido==$g->idgrupos) {
+                                                                                echo 'selected';
+                                                                            } 
+                                                                        }?>
+                                                                    ><?=$g->grupo?></option>
+                                                                    <?php endforeach?>
+                                                                    </select>
+                                                            </div>
+                                                            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                                                                <button type="submit" class="form-control btn btn-success">
+                                                                <p class="text-center">Cargar Préstamos</p> 
+                                                            </div>
+                                                        </div>
+                                            </div>
+                                                        
+                                    </form>  <!--FINcombobox para seleccionar  grupo-->
+                            </div>            
+                                       <!--Se cargan cada registro de prestamo-->     
+                            <br>
+                                <?php if (isset($prestamos) ):
+                                //se crea un boton para cada registro de prestamo?>
+                               
+                                            <?php foreach($prestamos as $presta):?>
+                                <div class="row m-0 p-0">        
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-0 p-0"> 
+                                        <button class="row   btn-primary  form-control collapsed" aria-expanded="false"  data-bs-toggle="collapse" href="#info<?=$presta->idPrestamos?>">
+                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si ">Folio: <?=$presta->idPrestamos ?>  </div>
+                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si  "><h6 class="text-center"> <?=$presta->nombre." ".$presta->apaterno." ".$presta->amaterno ?></h6>  </div>
+                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si "><?=$presta->fecha_prestamo?>   </div>
+                                            
+                                        </button>  
+                                               
+                                        
+                                     </div>
+                                      
+                                </div>
+                                <br>
+                            <!--row colapsable-->
+                            <div class=" row collapse "  id="info<?=$presta->idPrestamos?>">
+                          
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-0 p-0">
+                                    <div class="row m-0 p-0">
+                                              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 m-0 p-0">  <h6 class="text-center"> Detalles:</h6></div> 
+                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si m-0 p-0"> <p class="text-center"><b><?=$presta->titulo?></b> </p></div>
+                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si m-0 p-0"><p class="text-center"><b>Ejemplar: <?=$presta->ejemplar ?></b></p></div> 
+                                    </div>
+                                    <!--se cargan iconos de accion-->
+                                    
+                                    <div class="row m-0 p-0">
+                                    <hr>
+                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si m-0 p-0"> 
+                                            <h6 class="text-center">Acciones:</h6>
+                                            </div>
+                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si m-0 p-0"> 
+                                              <p class="text-center"> <a href="devolverLibro.php?id=<?=$presta->idPrestamos?>">
+                                                <img class="icono-seguimiento"
+                                            src="../../img/icons/borrow.png" alt="otra nota">    </a></p> 
+                                           <p class="text-center">Devolver </p>
+                                            </div>
+                                            
+                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 si m-0 p-0"> <p class="text-center">
+                                               <p class="text-center"> <a href="seguimientoCaso.php?id=<?=$presta->idPrestamos?>">
+                                                <img class="icono-seguimiento"
+                                            src="../../img/icons/delete.png" alt="otra nota">    </a></p>
+                                           <p class="text-center">Eliminar Préstamo</p>
+                                             </div> 
+                                     </div>
+                                    <!--temina carga iconos de accion-->
+                                </div> 
+                            </div><!--TERMINA row colapsable-->
+                            
+                            <?php endforeach?>
+                            
+                            <?php endif?> 
+                   </div> <!--Final contenido central--> 
+<!-- termina aqui -->
             <!--contenedor central -->
-          </div>
+          
        
          
 <!-- FIN CENTRO -->
@@ -77,12 +172,91 @@ $estadisticas =  buscaEstadisticasPrestamos();?>
      
 
         
-        <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 form-control">
+        <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 form-control2">
     <!--contenedor derecha -->
-contenedor derecha
+                 <!--Mostrar estadisticas --> 
+               <h5 class="text-center"><b> Algunos datos importantes </b></h4>
+                     <br>
+                     <div class="row form-control m-0 p-0"  style="background:#FFFF99; font-size: small; border-radius: 20px;">
+                        <!--inicio libros prestados-->
+                          <h6 class="text-center">Libros  Prestados Actualmente</h6>
+                          <hr class="m-0 p-0">
+                                                <?php if(isset($estadisticas)):?>
+                                                    <?php foreach($estadisticas as $e):?>
+                                                    <label for=""><b> <h3 class=" text-center blink success"><?=$e['prestados']->prestados?></h3></b></label>                                                     
+                                                       </div>  
+                                                       <br> 
+                                                        <!--inicio libros prestados-->
+                                                        <div class="row form-control m-0 p-0"  style="background:#FFFF99; font-size: small; border-radius: 20px;">
+                                                     <h6 class="text-center">Total Prestados en el ciclo escolar</h6>
+                                                      <hr class="m-0 p-0">
+                                            
+                                                      <label for=""><h3 class="text-center"><b> <?=$e['Total'][0]->prestados?></h3></b></label>
+                                                      
+                                                       </div>  
+                                                       <br>                       
+                        <!--final libros prestados-->
+                          <!--titulos mas prestados-->
+                          <div class="row form-control m-0 p-0"  style="background:#FFFF99; font-size: small; border-radius: 20px;">
+                            <h4 class="text-center">Top 10</h4>
+                          <h4 class="text-center">Títulos mas leídos</h4>
+                          <hr>
+                                                
+                                                        <?php $x=0; ?>
+                                                        <?php  foreach($e['xTitulo'] as $xtitulo):?>
+                                                            <div class="row m-0 p-0" >
+                                                            <?php if($xtitulo != ""):?>
+                                                                <?php $x++; 
+                                                                if($x<11): ?>
+                                                            <div class="col-lg-5 col-md-5 col-xs-5 col-sm-8 p-0">
+                                                             <label for=""><b> <p><?=$xtitulo['titulo']?>:</div>
+                                                            <div class="col-lg-5 col-md-5 col-xs-6 col-sm-3 p-0">
+                                                            <p class="text-center">  <?=$xtitulo['prestamos']?></p></b></label></p>
+                                                            </div>
+                                                            <?php endif?>
+                                                             <?php endif?>
+                                                             </div>
+                                                        <?php endforeach?>
+
+                            </div>
+                            <br>
+                              <!--grupos con mas prestamos-->
+                          <div class="row form-control  " style="background:#FFFF99; font-size: small; border-radius: 20px;">
+                        <h4 class="text-center">Grupos con  más préstamos</h4>
+                          <hr>
+                                                
+                                        
+                                                       <?php $i=0; ?>
+                                                        <?php foreach($e['xGrupo'] as $xGrupo):?>
+                                                            <div class="row m-0 p-0">
+                                                            <?php if($xGrupo != ""):?>
+                                                                <?php $i++; 
+                                                                if($i<6): ?>
+                                                            <div class="col-lg-5 col-md-5 col-xs-7 col-sm-6 p-0">
+                                                             <p><b> <?=$xGrupo['Grupo']?>:</b></div>
+                                                            <div class="col-lg-7 col-md-6 col-xs-4 col-sm-5 p-0">
+                                                            <p class="text-center">  <b> <?=$xGrupo['prestamos']?></b></p>
+                                                            </div>
+                                                           
+                                                            
+                                                            <?php endif?>
+                                                             <?php endif?>
+                                                             </div>
+                                                        <?php endforeach?>
+                                                     
+                            </div>
+                                                        <?php endforeach?>
+                                                    <?php endif?>
+
+                        <!--final titulos mas prestados-->
+
+
+                     </div>
     <!--contenedor derecha -->
 
         </div>
     </div>
 </div>
-   
+
+    <script> </script>
+<?php require '../complementos/footer_2.php';?>
