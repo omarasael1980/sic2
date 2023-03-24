e8097a51bae3f86389e0cd9965caf795a8d23c5f
Omar Asael Hernandez Montiel <omarasael@nube.unadmexico.mx>
2023-03-24 07:47:18 -0700

    Cambio de devolverLibro menu lateral

--- vistas/biblioteca/devolverLibro.php
+++ vistas/biblioteca/devolverLibro.php
@@ -1,5 +1,5 @@
-
 <?php
+
 include '../../modelo/usuarios/usuarios.php';
 require '../complementos/header_2.php';
 require '../complementos/nav_2.php';
@@ -13,35 +13,93 @@ if(!isset($_SESSION['user']) || !in_array('Biblioteca',$_SESSION['user']->perm))
 $hoy = date("Y-m-d");
 $idPrestamo =$_GET['id'];
 $prestamo = buscaPrestamo($idPrestamo);
+$espacios = "        ";
 ?>
 
+
 <!-- body  -->
-<div class="container"><!--inicia contenedor principal-->
+<div class="container-fluid">
+    
+<div class="row">
+    <!--contenedor general -->
+     <!--contenedor izquierda -->
+     <br>
+     <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
+       <div class="row">
+                 <center><a href="../../vistas/biblioteca/bprincipal.php">
+                 <img class="img-menu" src="../../img/icons/libreria.jpg" alt="biblioteca"></a></center>
+        </div>
+        <div class="row">
+                
+                    <h4 class="text-center">Biblioteca</h4>
+                
+        </div>
+        <div class="row">
+                <h1 class="text-center">Menú</h1>
+                <div class="list-group">
+                  
+                         <!--Menu desplegable-->
+                         <a href="bprincipal.php" class=" btn  list-group-item text-center list-group-item-action " aria-current="true">
+                         <p> <i class="fa-solid fa-house"></i><?=$espacios?>Principal</p>  </a>
+                         <br>   <a href="nuevoPrestamo.php" class=" btn  list-group-item text-center list-group-item-action " aria-current="true">
+                         <p> <i class="fa-solid fa-circle-plus"></i> <?=$espacios?>Nuevo Préstamo</p>  </a>
+                         <br>
+                        <a class="list-group-item text-center list-group-item-action" href="historialPrestamos.php"><p><img class="logos-enfermeria"
+                                        src="../../img/icons/history.png" alt=""><?=$espacios?> Historial </p></a>
+                        <br> <a href="binventario.php" class="list-group-item text-center list-group-item-action"><p> <img class="logos-enfermeria"
+                                        src="../../img/icons/inventory.png" alt=""> <?=$espacios?>Inventario </p> </a>
+                        <br> <a href="estadisticas.php" class="list-group-item text-center list-group-item-action"><p><i class="fa-solid fa-chart-simple"></i> <?=$espacios?>Estadísticas</p></a>
+                       
+                       
+          </div>
+                
+        </div>
+        </div> 
+          
+<!-- termina barra lateral izquierda -->
+    
+      
+        <!--contenedor central -->  <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 ">
+        
+        <div class="container"><!--inicia contenedor principal-->
     <div class="row"><!--inicia row principal-->
-    <center><h1>Devolver libro</h1></center> <br><br><br>
+    <h1 class="text-center">Devolver libro</h1>
             <div class="col-lg-3 col-md-2 col-sm-2 col-xs-0"> </div>
             <!--Contenido principal de la pagina-->
             <div class="col-lg-6 col-md-8 col-sm-8 col-xs-12"> 
                         <?php foreach($prestamo as $p):?>
                     <form autocomplete ="off" action="../../controlador/biblioteca/devolverLibro.php" class="form-control2" method="post">
-                  <br>  <b>Folio:</b>   <input class="form-control" type="text" name="idPrestamo" readonly value="<?=$p->idPrestamos?>">
-                  <br><b>Título:</b>   <input class="form-control" type="text" name="titulo" readonly value="<?=$p->titulo?>">
-                  <br> <b>Ejemplar:</b>   <input class="form-control" type="text" name="ejemplar" readonly value="<?=$p->ejemplar?>">
+                    <b>Folio:</b>   <input class="form-control" type="text" name="idPrestamo" readonly value="<?=$p->idPrestamos?>">
+                  <b>Título:</b>   <input class="form-control" type="text" name="titulo" readonly value="<?=$p->titulo?>">
+                  <b>Ejemplar:</b>   <input class="form-control" type="text" name="ejemplar" readonly value="<?=$p->ejemplar?>">
                      <input class="form-control" type="hidden" name="idEjemplar" value="<?=$p->idEjemplar?>">
-                  <br>  <b>Fecha de Préstamo:</b>   <input class="form-control" type="date" name="fprestamo" readonly value="<?=$p->fecha_prestamo?>">
-                  <br>  <b>Fecha de regreso:</b>   <input class="form-control" type="date" name="fregreso"  value="<?=$hoy?>">
-                  <br>  <b>Observaciones:</b>   <input class="form-control" type="text" name="observaciones"  value="" >
-                  <br> <button class="form-control btn btn-success">Finalizar Regreso de libro</button>
+                   <b>Fecha de Préstamo:</b>   <input class="form-control" type="date" name="fprestamo" readonly value="<?=$p->fecha_prestamo?>">
+                    <b>Fecha de regreso:</b>   <input class="form-control" type="date" name="fregreso"  value="<?=$hoy?>">
+                    <b>Observaciones:</b>   <input class="form-control" type="text" name="observaciones"  value="" >
+                 <br><button class="form-control btn btn-success">Finalizar Regreso de libro</button>
                     </form>
 
                     <?php endforeach?>
             </div>
             <!--termina contenido principal de la pagina-->
     </div><!--termina  row principall-->
-</div>  <!--termina contenedor principal-->   
-           
-           <!--js-->
-           
-               <script src="../../js/peticiones.js">        </script>
-               <script src="../../js/colapsables.js"></script>
+</div>  <!--termina contenedor principal-->          
+<!--contenedor central --> </div>
+     
+
+        
+        <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 form-control2">
+    <!--contenedor derecha -->
+                 <!--Mostrar estadisticas --> 
+             
+
+
+                     
+    <!--contenedor derecha -->
+
+        </div>
+    </div>
+</div>
+
+              
            <?php require '../complementos/footer_2.php';?>
--- vistas/biblioteca/plantillalibro.php
+++ vistas/biblioteca/plantillalibro.php
@@ -1,5 +1,5 @@
-
 <?php
+
 include '../../modelo/usuarios/usuarios.php';
 require '../complementos/header_2.php';
 require '../complementos/nav_2.php';
@@ -10,43 +10,85 @@ require "../../modelo/biblioteca/comunesBiblioteca.php";
 if(!isset($_SESSION['user']) || !in_array('Biblioteca',$_SESSION['user']->perm)){
     header("Location:../../");
 }
+$grupos =buscarGrupoS();
+if(isset ($_POST['grupo'])){
+    $grupoElegido = $_POST['grupo'];
+    $prestamos =buscaPrestamosActivosGrupo($grupoElegido);
+    if($prestamos == ""){
+        unset($prestamos);
+    } 
+}
+  $espacios = "        ";
+$estadisticas =  buscaEstadisticasPrestamos();?>
 
-?>
 
 <!-- body  -->
-<div class="container"><!--inicia contenedor principal-->
-    <div class="row"><!--inicia row principal-->
-    <!--Contenido principal de la pagina-->
-    <!--inicia busqueda por alumno-->
-    <?php if(isset($busca)):?>
-        <?php if($busca=="alumno"):?>
-    <div class="row">
-                        <div class="col-lg-2 col-md-1 col-sm-0 col-xs-0"></div>
-                        <center>  <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12">
-                                 <form action="nuevoPrestamo.php"  class="form-control"  method="post" autocomplete="off">
-                                     <div>
-                                        <label for="alumno">Buscar Alumno:</label>
-                                        <input type="text" class="form-control" name="alumno"  id="alumno">
-                                        <ul id="lista">                                                    
-                                                </ul>
-                                     </div>
-                                    <button class="btn btn-success">Cargar historial del alumno</button>
-                                 </form>
+<div class="container-fluid">
+    
+<div class="row">
+    <!--contenedor general -->
+     <!--contenedor izquierda -->
+     <br>
+     <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
+       <div class="row">
+                 <center><a href="../../vistas/biblioteca/bprincipal.php">
+                 <img class="img-menu" src="../../img/icons/libreria.jpg" alt="biblioteca"></a></center>
+        </div>
+        <div class="row">
+                
+                    <h4 class="text-center">Biblioteca</h4>
+                
+        </div>
+        <div class="row">
+                <h1 class="text-center">Menú</h1>
+                <div class="list-group">
+                  
+                         <!--Menu desplegable-->
+                         <a href="bprincipal.php" class=" btn btn-primary list-group-item text-center list-group-item-action " aria-current="true">
+                         <p> <i class="fa-solid fa-house"></i><?=$espacios?>Principal</p>  </a>
+                         <br>   <a href="nuevoPrestamo.php" class=" btn  list-group-item text-center list-group-item-action " aria-current="true">
+                         <p> <i class="fa-solid fa-circle-plus"></i> <?=$espacios?>Nuevo Préstamo</p>  </a>
+                         <br>
+                        <a class="list-group-item text-center list-group-item-action" href="historialPrestamos.php"><p><img class="logos-enfermeria"
+                                        src="../../img/icons/history.png" alt=""><?=$espacios?> Historial </p></a>
+                        <br> <a href="binventario.php" class="list-group-item text-center list-group-item-action"><p> <img class="logos-enfermeria"
+                                        src="../../img/icons/inventory.png" alt=""> <?=$espacios?>Inventario </p> </a>
+                        <br> <a href="estadisticas.php" class="list-group-item text-center list-group-item-action"><p><i class="fa-solid fa-chart-simple"></i> <?=$espacios?>Estadísticas</p></a>
+                       
+                       
+          </div>
+                
+        </div>
+        </div> 
+          
+<!-- termina barra lateral izquierda -->
+    
+      
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 ">
+          <!--contenedor central -->
 
-              
-                        </div></center>
 
-            </div>
-            <?php endif;?>
-<?php endif;?>
+            <!--contenedor central -->
+          
+       
+         
+<!-- FIN CENTRO -->
+<!--contenedor central -->
+     
+
+        
+        <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 form-control2">
+    <!--contenedor derecha -->
+                 <!--Mostrar estadisticas --> 
+             
 
 
-    <!--termina contenido principal de la pagina-->
-    </div><!--termina  row principall-->
-</div>  <!--termina contenedor principal-->   
-           
-           <!--js-->
-           
-               <script src="../../js/peticiones.js">        </script>
-               <script src="../../js/colapsables.js"></script>
-           <?php require '../complementos/footer_2.php';?>
+                     
+    <!--contenedor derecha -->
+
+        </div>
+    </div>
+</div>
+
+    <script> </script>
+<?php require '../complementos/footer_2.php';?>
